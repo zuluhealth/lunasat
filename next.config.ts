172301ce3fca7225d7753bd41100f79547b21a2e
MGH <fouad.b.younes@mgholding.com>
2026-07-03 12:17:19 +0300

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
+  output: "standalone",
+  turbopack: {
+    root: path.resolve(__dirname),
+  },
+  outputFileTracingRoot: path.resolve(__dirname),
   images: {
     remotePatterns: [
       {
